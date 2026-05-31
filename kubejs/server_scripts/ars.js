1ce0989f33573a381e82f04dbde4429aaa0354e5
Olliemyers4 <olliemyers4@gmail.com>
2026-05-31 12:14:01 +0100

    Tier2 ars_nouveau

--- kubejs/server_scripts/ars.js
+++ kubejs/server_scripts/ars.js
@@ -74,4 +74,95 @@ ServerEvents.recipes(event => {
         27  // exp cost 3lvl
     );
 
+    // **Need T2**
+
+    // Mage's Spell book
+    event.remove({id:'ars_nouveau:apprentice_book_upgrade'});
+    event.shapeless(Item.of('ars_nouveau:apprentice_spell_book',1),['ars_nouveau:novice_spell_book','kubejs:ars_core_t2','3x minecraft:diamond', '2x minecraft:blaze_rod', '2x minecraft:quartz_block']);
+
+    // Arc projectile rune
+    event.remove({id:'ars_nouveau:glyph_arc_projectile'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_arc_projectile', // output item (glyph)
+        [Ingredient.of('minecraft:arrow'),Ingredient.of('minecraft:snowball'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('minecraft:ender_pearl'),Ingredient.of('minecraft:slime_ball')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // AOE rune
+    event.remove({id:'ars_nouveau:glyph_aoe'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_aoe', // output item (glyph)
+        [Ingredient.of('minecraft:firework_star'),Ingredient.of('kubejs:ars_core_t2')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // Accelerate rune
+    event.remove({id:'ars_nouveau:glyph_accelerate'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_accelerate', // output item (glyph)
+        [Ingredient.of('minecraft:powered_rail'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('minecraft:clock'),Ingredient.of('minecraft:sugar')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // Dampen rune
+    event.remove({id:'ars_nouveau:glyph_dampen'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_dampen', // output item (glyph)
+        [Ingredient.of('minecraft:nether_brick'),Ingredient.of('kubejs:ars_core_t2')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // Decelerate rune
+    event.remove({id:'ars_nouveau:glyph_decelerate'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_decelerate', // output item (glyph)
+        [Ingredient.of('minecraft:clock'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('minecraft:soul_sand'),Ingredient.of('minecraft:cobweb')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // Extend Time rune
+    event.remove({id:'ars_nouveau:glyph_extend_time'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_extend_time', // output item (glyph)
+        [Ingredient.of('minecraft:clock'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('minecraft:redstone_block')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // extract rune
+    event.remove({id:'ars_nouveau:glyph_extract'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_extract', // output item (glyph)
+        [Ingredient.of('minecraft:emerald'),Ingredient.of('kubejs:ars_core_t2')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // luck rune
+    event.remove({id:'ars_nouveau:glyph_luck'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_luck', // output item (glyph)
+        [Ingredient.of('minecraft:rabbit_foot'),Ingredient.of('kubejs:ars_core_t2')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // pierce rune
+    event.remove({id:'ars_nouveau:glyph_pierce'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_pierce', // output item (glyph)
+        [Ingredient.of('minecraft:arrow'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('ars_nouveau:wilden_spike')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // reduce time rune
+    event.remove({id:'ars_nouveau:glyph_reduce_time'});
+    event.recipes.ars_nouveau.glyph(
+        'ars_nouveau:glyph_reduce_time', // output item (glyph)
+        [Ingredient.of('minecraft:clock'),Ingredient.of('kubejs:ars_core_t2'),Ingredient.of('minecraft:glowstone_dust')], // input items
+        55  // exp cost 5lvl
+    );
+
+    // Spell Prism
+    event.remove({id:'ars_nouveau:spell_prism'});
+    event.shaped(Item.of('ars_nouveau:spell_prism',1), ['GPG','PTP','GPG'], {P:'ars_nouveau:archwood_planks',T:'kubejs:ars_core_t2',G:'minecraft:gold_ingot'});
+
+
 });
